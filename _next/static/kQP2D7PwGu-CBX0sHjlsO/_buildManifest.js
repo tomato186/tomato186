ab3b2d6db574f0cd75fbbc8488a10a597714bdc2
tomato186 <rafed0770578@gmail.com>
2025-10-17 01:37:56 +0300

    Updates

---
+++ _next/static/kQP2D7PwGu-CBX0sHjlsO/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/40258d3f6248f099.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/.//_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
